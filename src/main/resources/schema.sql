2da84c16ccdcc5fe3798368c2f0b10b275313c1f
김호준 <corojoon93@naver.com>
2023-06-24 16:20:54 +0900

    fix: SQL DDL 변경
    - UNSIGNED 삭제
    - member 테이블 변경
       - profile_url 컬럼 추가
       - bucket_data 테이블 삭제, profile_url 컬럼이 대체
       - profile_url = 네이버클라우드 도메인 + 버킷명 + 회원 id 조합으로 구성. 도메인명 길이는 대략 30글자. 버킷명은 최대 64자, 회원 id 는 BIGINT 타입으로 최대 20자. 합쳐서 대략 120글자 => 여유있게 200글자로 구성
    - election 테이블 변경
       - 유튜브 정책 변경 가능성이 있으므로 youtube_share_id 길이 변경 (11→20)
    - game_info 테이블 변경
       - 새로운 포지션 추가 가능성이 존재하므로 position 컬럼 변경 (enum → varchar)
       - 팀 색깔 변경 가능성이 존재하므로 team 컬럼 타입 변경 (enum → varchar)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,11 @@
 -- 리그오브레전드 계정 테이블
 CREATE TABLE game_account
 (
-    id              BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    login_id        VARCHAR(24),
-    nick_name       VARCHAR(16),
+    id              BIGINT NOT NULL AUTO_INCREMENT,
+    login_id        VARCHAR(24) UNIQUE,
+    nickname        VARCHAR(16) UNIQUE,
     rank_tier_group VARCHAR(16),
-    rank_tier_level TINYINT UNSIGNED CHECK (0 < rank_tier_level AND rank_tier_level < 5),
-
-    PRIMARY KEY (id)
-);
-
--- 네이버클라우드 버킷 데이터 테이블
-CREATE TABLE bucket_data
-(
-    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    rank_tier_level TINYINT CHECK (0 < rank_tier_level AND rank_tier_level < 5),
 
     PRIMARY KEY (id)
 );
@@ -21,44 +13,64 @@ CREATE TABLE bucket_data
 -- 회원 테이블
 CREATE TABLE member
 (
-    id               BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    game_account_id  BIGINT UNSIGNED,
-    profile_image_id BIGINT UNSIGNED,
-    email            VARCHAR(320),
-    pwd              VARCHAR(32),
-    judge_point      INTEGER UNSIGNED,
+    id              BIGINT NOT NULL AUTO_INCREMENT,
+    game_account_id BIGINT,
+    profile_url     VARCHAR(200),
+    email           VARCHAR(320),
+    `password`      VARCHAR(32),
+    judge_point     INT        DEFAULT 0,
+    deleted         TINYINT(1) DEFAULT 0,
 
     PRIMARY KEY (id),
 
     FOREIGN KEY (game_account_id)
-        REFERENCES game_account (id),
-    FOREIGN KEY (profile_image_id)
-        REFERENCES bucket_data (id)
+        REFERENCES game_account (id)
+);
+
+-- 인게임 정보 테이블
+CREATE TABLE game_info
+(
+    id        BIGINT NOT NULL AUTO_INCREMENT,
+
+    team      VARCHAR(8),
+    position  VARCHAR(16),
+    nickname  VARCHAR(16),
+    champion  VARCHAR(8),
+    `kill`    TINYINT,
+    death     TINYINT,
+    assist    TINYINT,
+    played_at TIMESTAMP,
+
+    PRIMARY KEY (id)
 );
 
 -- 논쟁거리 재판(선거) 테이블
 CREATE TABLE election
 (
-    id               BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id               BIGINT NOT NULL AUTO_INCREMENT,
+    game_info_id     BIGINT,
     election_no      VARCHAR(64),
-    status           VARCHAR(8),
+    `status`         VARCHAR(8),
     contents         VARCHAR(1000),
-    cost             INT UNSIGNED,
-    youtube_share_id VARCHAR(11),
+    cost             INT,
+    youtube_share_id VARCHAR(20),
     modified_at      TIMESTAMP,
     ended_at         TIMESTAMP,
 
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+
+    FOREIGN KEY (game_info_id)
+        REFERENCES game_info (id)
 );
 
 -- 논쟁거리 재판(선거) 후보자 테이블
 CREATE TABLE candidate
 (
-    id           BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    election_id  BIGINT UNSIGNED NOT NULL,
-    member_id    BIGINT UNSIGNED NOT NULL,
-    voted_count  BIGINT UNSIGNED,
-    voted_status ENUM('WIN','LOSE'),
+    id           BIGINT NOT NULL AUTO_INCREMENT,
+    election_id  BIGINT NOT NULL,
+    member_id    BIGINT NOT NULL,
+    voted_count  BIGINT,
+    voted_status ENUM ('WIN','LOSE'),
 
     PRIMARY KEY (id),
 
@@ -68,35 +80,18 @@ CREATE TABLE candidate
         REFERENCES member (id)
 );
 
--- 인게임 정보 테이블
-CREATE TABLE game_info
-(
-    id       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-
-    team     ENUM('RED', 'BLUE'),
-    position ENUM('TOP', 'JUNGLE', 'MID', 'ADC', 'SUPPORT'),
-    champion VARCHAR(8),
-    kill     TINYINT,
-    death    TINYINT,
-    assist   TINYINT,
-
-    PRIMARY KEY (id)
-);
-
 -- 논쟁거리 재판(선거) 후보자 의견 테이블
 CREATE TABLE opinion
 (
-    id           BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    candidate_id BIGINT UNSIGNED NOT NULL,
-    game_info_id BIGINT UNSIGNED NOT NULL,
+    id           BIGINT NOT NULL AUTO_INCREMENT,
+    candidate_id BIGINT NOT NULL,
+    game_info_id BIGINT NOT NULL,
     contents     VARCHAR(1000),
     modified_at  TIMESTAMP,
-    is_agreed    TINYINT(1),
+    is_agreed    BOOLEAN,
 
     FOREIGN KEY (candidate_id)
         REFERENCES candidate (id),
-    FOREIGN KEY (game_info_id)
-        REFERENCES game_info (id),
 
     PRIMARY KEY (id)
 );
@@ -104,9 +99,9 @@ CREATE TABLE opinion
 -- 논쟁거리 재판(선거) 투표 테이블
 CREATE TABLE vote
 (
-    id           BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    member_id    BIGINT UNSIGNED,
-    candidate_id BIGINT UNSIGNED,
+    id           BIGINT NOT NULL AUTO_INCREMENT,
+    member_id    BIGINT,
+    candidate_id BIGINT,
 
     FOREIGN KEY (member_id)
         REFERENCES member (id),
