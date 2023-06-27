63d601b43efb09197531e223826e02d924c7e5dd
김호준 <corojoon93@naver.com>
2023-06-27 23:47:36 +0900

    feat: Member 도메인 추가

---
+++ src/main/java/edu/flab/global/vo/JudgePoint.java
@@ -0,0 +1,14 @@
+package edu.flab.global.vo;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@EqualsAndHashCode
+@Builder
+@Getter
+public class JudgePoint {
+	public static JudgePoint ZERO = new JudgePoint(0);
+
+	private final Integer point;
+}
