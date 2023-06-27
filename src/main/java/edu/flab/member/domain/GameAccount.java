6e8034faabb53646623aff7abe3368e8a4a545f9
김호준 <corojoon93@naver.com>
2023-06-27 23:21:55 +0900

    fix: GameAccount 도메인 수정
    - memberId 필드 추가
    - GameAccount(Long, Long, String, String) 생성자 추가 => Mybatis ResultMap 에서 사용하기 위한 생성자

--- src/main/java/edu/flab/member/domain/GameAccount.java
+++ src/main/java/edu/flab/member/domain/GameAccount.java
@@ -1,18 +1,28 @@
 package edu.flab.member.domain;
 
 import edu.flab.global.vo.RankTier;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 @ToString
-@Getter
 @EqualsAndHashCode
+@AllArgsConstructor
 @Builder
+@Getter
 public class GameAccount {
 	private Long id;
-	private String loginId;
+	private Long memberId;
 	private String nickname;
+	private String loginId;
 	private RankTier rankTier;
+
+	public GameAccount(Long id, Long memberId, String nickname, String loginId) {
+		this.id = id;
+		this.memberId = memberId;
+		this.nickname = nickname;
+		this.loginId = loginId;
+	}
 }
