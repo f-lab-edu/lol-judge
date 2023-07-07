1c6d875b336eb1be70b04bc1e4d608c99600ebf7
김호준 <corojoon93@naver.com>
2023-07-07 23:54:21 +0900

    fix: 암호화 빈 설정 클래스 이름 변경

--- src/main/java/edu/flab/CryptoBeanConfiguration.java
+++ src/main/java/edu/flab/CryptoBeanConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class BeanConfiguration {
+public class CryptoBeanConfiguration {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
