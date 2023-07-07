7af2f1be31a76cb47c04ff03fff3acc0604bf339
김호준 <corojoon93@naver.com>
2023-07-07 23:55:20 +0900

    feat: Member 클래스 validation

--- src/main/java/edu/flab/member/domain/Member.java
+++ src/main/java/edu/flab/member/domain/Member.java
@@ -1,6 +1,11 @@
 package edu.flab.member.domain;
 
+import org.hibernate.validator.constraints.URL;
+
+import edu.flab.global.validation.Password;
 import edu.flab.global.vo.JudgePoint;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Builder.Default;
@@ -15,8 +20,17 @@ import lombok.ToString;
 @Getter
 public class Member {
 	private Long id;
+
+	@NotNull
+	@Email(message = "이메일 형식만 입력 가능합니다")
 	private String email;
+
+	@NotNull
+	@Password(message = "패스워드 규칙은 영소문자, 영대문자, 특수문자를 포함한 최소 8글자입니다")
 	private String password;
+
+	@NotNull
+	@URL(message = "URL 형식만 입력 가능합니다")
 	private String profileUrl;
 	private boolean deleted;
 
