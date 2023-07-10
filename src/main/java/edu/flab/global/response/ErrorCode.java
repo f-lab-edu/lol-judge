effae6a1c8401031871c1544c2b83718d71418dd
김호준 <corojoon93@naver.com>
2023-07-11 00:45:55 +0900

    fix: ErrorCode 에러코드 필드 타입 변경 (int → httpStatusCode)

--- src/main/java/edu/flab/global/response/ErrorCode.java
+++ src/main/java/edu/flab/global/response/ErrorCode.java
@@ -1,19 +1,22 @@
 package edu.flab.global.response;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
 import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
 	// Common
-	ACCESS_DENIED(401, "C001", "Access is Denied"),
-	INVALID_INPUT_VALUE(400, "C002", "Invalid input value"),
-	METHOD_NOT_ALLOWED(405, "C003", "Not supported method");
+	ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "C001", "Access is Denied"),
+	INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "C002", "Invalid input value"),
+	METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C003", "Not supported method");
 
-	private final int httpStatusCode;
+	private final HttpStatusCode httpStatusCode;
 	private final String code;
 	private final String message;
 
-	ErrorCode(final int httpStatusCode, final String code, final String message) {
+	ErrorCode(final HttpStatusCode httpStatusCode, final String code, final String message) {
 		this.httpStatusCode = httpStatusCode;
 		this.code = code;
 		this.message = message;
