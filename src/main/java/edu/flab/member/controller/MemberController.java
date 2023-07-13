442024f38f15d805bc8127c80280d635cf32c904
korjun1993 <corojoon93@naver.com>
2023-07-13 23:14:24 +0900

    feat: 로그인 구현

--- src/main/java/edu/flab/member/controller/MemberController.java
+++ src/main/java/edu/flab/member/controller/MemberController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.flab.member.dto.MemberLoginDto;
 import edu.flab.member.dto.MemberSignUpDto;
+import edu.flab.member.service.MemberLoginService;
 import edu.flab.member.service.MemberSignUpService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -15,10 +18,16 @@ import lombok.RequiredArgsConstructor;
 @RestController
 public class MemberController {
 	private final MemberSignUpService memberSignUpService;
+	private final MemberLoginService memberLoginService;
 
 	@ResponseStatus(HttpStatus.OK)
 	@PostMapping("/signUp")
 	public void signUp(@RequestBody @Valid MemberSignUpDto dto) {
 		memberSignUpService.signUp(dto);
+	}
+
+	@PostMapping("/login")
+	public void login(HttpServletRequest request, @RequestBody @Valid MemberLoginDto dto) {
+		memberLoginService.login(request, dto);
 	}
 }
