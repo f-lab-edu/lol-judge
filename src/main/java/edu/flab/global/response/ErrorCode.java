83557e0f73cb5a81a0cf60861d3a9ad898d9f582
korjun1993 <corojoon93@naver.com>
2023-07-17 00:41:52 +0900

    fix: code 필드 불필요하다고 판단하여 삭제

--- src/main/java/edu/flab/global/response/ErrorCode.java
+++ src/main/java/edu/flab/global/response/ErrorCode.java
@@ -8,20 +8,18 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 	// Common
-	ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "C001", "Access is Denied"),
-	INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "C002", "Invalid input value"),
-	METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C003", "Not supported method"),
+	ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "Access is Denied"),
+	INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "Invalid input value"),
+	METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "Not supported method"),
 
 	// Member
-	PASSWORD_NOT_VALID(HttpStatus.UNAUTHORIZED, "M002", "Invalid Password");
+	PASSWORD_NOT_VALID(HttpStatus.UNAUTHORIZED, "Invalid Password");
 
 	private final HttpStatusCode httpStatusCode;
-	private final String code;
 	private final String message;
 
-	ErrorCode(final HttpStatusCode httpStatusCode, final String code, final String message) {
+	ErrorCode(final HttpStatusCode httpStatusCode, final String message) {
 		this.httpStatusCode = httpStatusCode;
-		this.code = code;
 		this.message = message;
 	}
 }
