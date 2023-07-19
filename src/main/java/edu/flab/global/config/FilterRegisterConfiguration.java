cb273f55e6366403b4223a5c458daf620155f35f
korjun1993 <corojoon93@naver.com>
2023-07-19 20:30:53 +0900

    fix: FilterRegisterConfiguration Configuration 애노테이션 추가

--- src/main/java/edu/flab/global/config/FilterRegisterConfiguration.java
+++ src/main/java/edu/flab/global/config/FilterRegisterConfiguration.java
@@ -2,10 +2,12 @@ package edu.flab.global.config;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import edu.flab.global.filter.LoginCheckFilter;
 import jakarta.servlet.Filter;
 
+@Configuration
 public class FilterRegisterConfiguration {
 	@Bean
 	public FilterRegistrationBean<Filter> loginCheckFilter() {
