44792cf56d0ebc4451fc756e5578c9132fed1a6f
korjun1993 <corojoon93@naver.com>
2023-07-24 14:56:40 +0900

    멀티 모듈로 전환

---
+++ app/src/test/java/edu/flab/member/controller/MemberControllerTest.java
@@ -0,0 +1,161 @@
+package edu.flab.controller;
+
+import static edu.flab.member.domain.LolTier.Color.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.flab.global.controller.GlobalExceptionHandler;
+import edu.flab.member.controller.MemberController;
+import edu.flab.member.domain.LolTier;
+import edu.flab.member.domain.LolTierUtil;
+import edu.flab.member.dto.MemberLoginDto;
+import edu.flab.member.dto.MemberSignUpDto;
+
+@Tag("integration")
+@Transactional
+@SpringBootTest
+class MemberControllerTest {
+
+	@Autowired
+	private MemberController memberController;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	private MockMvc mock;
+
+	private final LolTier challenger = LolTierUtil.createHighTier(CHALLENGER, 1000);
+
+	@BeforeEach
+	void setUp() {
+		mock = MockMvcBuilders.standaloneSetup(memberController)
+			.setControllerAdvice(new GlobalExceptionHandler())
+			.build();
+	}
+
+	@Test
+	void 회원가입에_성공한다() throws Exception {
+		MemberSignUpDto signUpDto = MemberSignUpDto.builder()
+			.email("admin@example.com")
+			.password("aB#12345")
+			.profileUrl("https://cloud.example.com/bucket/profile_image.jpg")
+			.gameLoginId("lolId1234")
+			.nickname("hide on bush")
+			.lolTier(challenger)
+			.build();
+
+		mock.perform(MockMvcRequestBuilders.post("/signUp")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(toJson(signUpDto)))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andDo(MockMvcResultHandlers.print());
+	}
+
+	@Test
+	void 비밀번호규칙을_어겨서_회원가입에_실패한다() throws Exception {
+		MemberSignUpDto signUpDto = MemberSignUpDto.builder()
+			.email("admin@example.com")
+			.password("aB#")
+			.profileUrl("https://cloud.example.com/bucket/profile_image.jpg")
+			.gameLoginId("lolId1234")
+			.nickname("hide on bush")
+			.lolTier(challenger)
+			.build();
+
+		mock.perform(MockMvcRequestBuilders.post("/signUp")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(toJson(signUpDto)))
+			.andExpect(MockMvcResultMatchers.status().is4xxClientError())
+			.andDo(MockMvcResultHandlers.print());
+	}
+
+	@Test
+	void 회원가입_이후_로그인에_성공한다() throws Exception {
+		MemberSignUpDto signUpDto = MemberSignUpDto.builder()
+			.email("admin@example.com")
+			.password("aB#12345")
+			.profileUrl("https://cloud.example.com/bucket/profile_image.jpg")
+			.gameLoginId("lolId1234")
+			.nickname("hide on bush")
+			.lolTier(LolTierUtil.createHighTier(CHALLENGER, 1400))
+			.build();
+
+		MemberLoginDto loginDto = MemberLoginDto.builder()
+			.email(signUpDto.getEmail())
+			.password(signUpDto.getPassword())
+			.build();
+
+		mock.perform(MockMvcRequestBuilders.post("/signUp")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(toJson(signUpDto)))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andDo(MockMvcResultHandlers.print());
+
+		mock.perform(MockMvcRequestBuilders.post("/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(toJson(loginDto)))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andDo(MockMvcResultHandlers.print());
+	}
+
+	@Test
+	void 회원가입_이후_비밀번호를_틀려_로그인에_실패한다() throws Exception {
+		MemberSignUpDto signUpDto = MemberSignUpDto.builder()
+			.email("admin@example.com")
+			.password("aB#12345")
+			.profileUrl("https://cloud.example.com/bucket/profile_image.jpg")
+			.gameLoginId("lolId1234")
+			.nickname("hide on bush")
+			.lolTier(LolTierUtil.createHighTier(CHALLENGER, 1400))
+			.build();
+
+		MemberLoginDto loginDto = MemberLoginDto.builder()
+			.email(signUpDto.getEmail())
+			.password("invalid password")
+			.build();
+
+		mock.perform(MockMvcRequestBuilders.post("/signUp")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(toJson(signUpDto)))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andDo(MockMvcResultHandlers.print());
+
+		mock.perform(MockMvcRequestBuilders.post("/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(toJson(loginDto)))
+			.andExpect(MockMvcResultMatchers.status().is4xxClientError())
+			.andDo(MockMvcResultHandlers.print());
+	}
+
+	@Test
+	void 존재하지않는_이메일를_입력하여_로그인에_실패한다() throws Exception {
+		MemberLoginDto loginDto = MemberLoginDto.builder()
+			.email("admin@example.com")
+			.password("aB#12345")
+			.build();
+
+		mock.perform(MockMvcRequestBuilders.post("/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(toJson(loginDto)))
+			.andExpect(MockMvcResultMatchers.status().is4xxClientError())
+			.andDo(MockMvcResultHandlers.print());
+	}
+
+	public <T> String toJson(T data) throws JsonProcessingException {
+		return objectMapper.writeValueAsString(data);
+	}
+}
---
+++ app/src/test/java/edu/flab/member/service/MemberLoginServiceTest.java
@@ -0,0 +1,62 @@
+package edu.flab.service;
+
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import edu.flab.global.exception.AuthenticationException;
+import edu.flab.member.domain.Member;
+import edu.flab.member.dto.MemberLoginDto;
+import edu.flab.member.repository.MemberMapper;
+import edu.flab.member.service.MemberLoginService;
+
+@ExtendWith(MockitoExtension.class)
+class MemberLoginServiceTest {
+
+	@Mock
+	private MemberMapper memberMapper;
+
+	@Mock
+	private PasswordEncoder passwordEncoder;
+
+	@InjectMocks
+	private MemberLoginService sut;
+
+	@Test
+	void 올바른_비밀번호를_입력하면_로그인에_성공한다() {
+		// given
+		Member member = Member.builder().email("example@example.com").password("1234").build();
+		MemberLoginDto dto = MemberLoginDto.builder().email(member.getEmail()).password(member.getPassword()).build();
+		MockHttpServletRequest request = new MockHttpServletRequest();
+
+		// when
+		Mockito.when(memberMapper.findActiveMemberByEmail(dto.getEmail())).thenReturn(Optional.of(member));
+		Mockito.when(passwordEncoder.matches(dto.getPassword(), member.getPassword())).thenReturn(true);
+
+		// then
+		sut.login(request, dto);
+	}
+
+	@Test
+	void 틀린_비밀번호를_입력하면_로그인에_실패한다() {
+		// given
+		Member member = Member.builder().email("example@example.com").password("1234").build();
+		MemberLoginDto dto = MemberLoginDto.builder().email(member.getEmail()).password(member.getPassword()).build();
+		MockHttpServletRequest request = new MockHttpServletRequest();
+
+		// when
+		Mockito.when(memberMapper.findActiveMemberByEmail(dto.getEmail())).thenReturn(Optional.of(member));
+		Mockito.when(passwordEncoder.matches(dto.getPassword(), member.getPassword())).thenReturn(false);
+
+		// then
+		Assertions.assertThatThrownBy(() -> sut.login(request, dto)).isInstanceOf(AuthenticationException.class);
+	}
+}
