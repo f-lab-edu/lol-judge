28fe829aca0f4780aadb6586da638e524616ef20
korjun1993 <corojoon93@naver.com>
2023-07-24 13:06:05 +0900

    fix: Assertions 추가

--- src/test/java/edu/flab/member/domain/LolTierUtilTest.java
+++ src/test/java/edu/flab/member/domain/LolTierUtilTest.java
@@ -5,59 +5,56 @@ import static org.assertj.core.api.Assertions.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 class LolTierUtilTest {
 
-	@Test
+	@ParameterizedTest
+	@EnumSource(value = LolTier.Color.class, names = {"MASTER", "GRAND_MASTER", "CHALLENGER"})
 	@DisplayName("상위 티어에 해당하는 티어는 [MASTER ~ CHALLENGER]이다")
-	void test1() {
-		LolTierUtil.highTier(LolTier.Color.MASTER, 500);
-		LolTierUtil.highTier(LolTier.Color.GRAND_MASTER, 1000);
-		LolTierUtil.highTier(LolTier.Color.CHALLENGER, 1500);
+	void test1(LolTier.Color color) {
+		Assertions.assertThatNoException().isThrownBy(() -> LolTierUtil.createHighTier(color, 1000));
 	}
 
 	@Test
 	@DisplayName("상위 티어에 해당하는 티어는 0점 이상의 랭크게임 포인트를 가질 수 있다")
 	void test2() {
-		Assertions.assertThatThrownBy(() -> LolTierUtil.highTier(LolTier.Color.MASTER, -1))
+		Assertions.assertThatThrownBy(() -> LolTierUtil.createHighTier(LolTier.Color.MASTER, -1))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@Test
 	@DisplayName("상위 티어에 해당하는 티어의 단계는 오직 0 하나 뿐이다")
 	void test3() {
-		LolTier master = LolTierUtil.highTier(LolTier.Color.MASTER, 500);
+		LolTier master = LolTierUtil.createHighTier(LolTier.Color.MASTER, 500);
 		Assertions.assertThat(master.getLevel()).isEqualTo(0);
 	}
 
-	@Test
+	@ParameterizedTest
+	@EnumSource(value = LolTier.Color.class, names = {"IRON", "BRONZE", "SILVER", "GOLD", "PLATINUM", "DIAMOND"})
 	@DisplayName("일반 티어에 해당하는 티어는 [IRON ~ DIAMOND]이다")
-	void test4() {
-		LolTierUtil.normalTier(LolTier.Color.IRON, 1, 100);
-		LolTierUtil.normalTier(LolTier.Color.BRONZE, 1, 100);
-		LolTierUtil.normalTier(LolTier.Color.SILVER, 1, 100);
-		LolTierUtil.normalTier(LolTier.Color.GOLD, 1, 100);
-		LolTierUtil.normalTier(LolTier.Color.PLATINUM, 1, 100);
-		LolTierUtil.normalTier(LolTier.Color.DIAMOND, 1, 100);
+	void test4(LolTier.Color color) {
+		Assertions.assertThatNoException().isThrownBy(() -> LolTierUtil.createNormalTier(color, 1, 100));
 	}
 
 	@Test
 	@DisplayName("일반 티어에 해당하는 티어는 [0 ~ 100]점 사이의 랭크게임 포인트를 가질 수 있다")
 	void test5() {
-		assertThatThrownBy(() -> LolTierUtil.normalTier(LolTier.Color.IRON, 1, -10))
+		assertThatThrownBy(() -> LolTierUtil.createNormalTier(LolTier.Color.IRON, 1, -10))
 			.isInstanceOf(IllegalArgumentException.class);
 
-		assertThatThrownBy(() -> LolTierUtil.normalTier(LolTier.Color.IRON, 1, 150))
+		assertThatThrownBy(() -> LolTierUtil.createNormalTier(LolTier.Color.IRON, 1, 150))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@Test
 	@DisplayName("일반 티어에 해당하는 티어는 [0 ~ 4] 사이의 단계를 가질 수 있다")
 	void test6() {
-		assertThatThrownBy(() -> LolTierUtil.normalTier(LolTier.Color.IRON, -1, 50))
+		assertThatThrownBy(() -> LolTierUtil.createNormalTier(LolTier.Color.IRON, -1, 50))
 			.isInstanceOf(IllegalArgumentException.class);
 
-		assertThatThrownBy(() -> LolTierUtil.normalTier(LolTier.Color.IRON, 5, 50))
+		assertThatThrownBy(() -> LolTierUtil.createNormalTier(LolTier.Color.IRON, 5, 50))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
