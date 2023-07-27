e98ce6f791564c21658b15623e4f006e3cda6f1f
korjun1993 <corojoon93@naver.com>
2023-07-27 13:17:15 +0900

    멀티 모듈 전환

---
+++ module-notification/src/main/java/edu/flab/NotificationProcessApplication.java
@@ -0,0 +1,11 @@
+package edu.flab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationProcessApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationProcessApplication.class, args);
+	}
+}
