0d90dae62d6a18bd4f01953ff64922ccc2f5f54b
korjun1993 <corojoon93@naver.com>
2023-07-27 18:17:29 +0900

    멀티 모듈 전환시, 누락된 파일 추가

---
+++ module-app/src/main/java/edu/flab/WebApplication.java
@@ -0,0 +1,13 @@
+package edu.flab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+public class WebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+}
