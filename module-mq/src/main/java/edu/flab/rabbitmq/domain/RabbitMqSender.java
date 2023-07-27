f4cd74eb2bfb2e7b569b8f0947208b54b823ef39
korjun1993 <corojoon93@naver.com>
2023-07-27 18:51:08 +0900

    feat: RabbitMq Message Sender

---
+++ module-mq/src/main/java/edu/flab/rabbitmq/domain/RabbitMqSender.java
@@ -0,0 +1,35 @@
+package edu.flab.rabbitmq.domain;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import org.springframework.stereotype.Component;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import edu.flab.rabbitmq.message.RabbitMqMessage;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class RabbitMqSender {
+	private final ConnectionFactory connectionFactory;
+
+	public RabbitMqSender(ConnectionFactory connectionFactory) {
+		this.connectionFactory = connectionFactory;
+	}
+
+	public void notify(RabbitMqMessage message) {
+		try (Connection connection = connectionFactory.newConnection();
+			 Channel channel = connection.createChannel()) {
+			channel.queueDeclare(message.getQueueName(), true, false, false, null);
+			channel.basicPublish("", message.getQueueName(), MessageProperties.PERSISTENT_TEXT_PLAIN,
+				message.toString().getBytes());
+		} catch (IOException | TimeoutException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
