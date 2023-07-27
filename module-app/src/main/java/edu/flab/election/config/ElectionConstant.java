adcb85133514254d3782cedc6a50723345b02169
korjun1993 <corojoon93@naver.com>
2023-07-27 18:13:18 +0900

    feat: Election 도메인

---
+++ module-app/src/main/java/edu/flab/election/config/ElectionConstant.java
@@ -0,0 +1,11 @@
+package edu.flab.election.config;
+
+public class ElectionConstant {
+	// 재판 비용
+	public static final int MIN_COST = 10;
+	public static final int MAX_COST = 5000;
+
+	// 재판 오픈 기간
+	public static final int MIN_DURATION = 30;
+	public static final int MAX_DURATION = 1440;
+}
