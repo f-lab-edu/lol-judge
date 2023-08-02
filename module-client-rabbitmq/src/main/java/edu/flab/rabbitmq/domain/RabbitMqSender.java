1fbd134805f31b4b03d17fc9a7bb4e8bd359bfe2
korjun1993 <corojoon93@naver.com>
2023-08-02 21:21:51 +0900

    fix: RabbitMqMessage 클래스 Generic 적용

--- module-client-rabbitmq/src/main/java/edu/flab/rabbitmq/domain/RabbitMqSender.java
+++ module-client-rabbitmq/src/main/java/edu/flab/rabbitmq/domain/RabbitMqSender.java
@@ -7,29 +7,30 @@ import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
 
 import edu.flab.rabbitmq.message.RabbitMqMessage;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class RabbitMqSender {
+	private final ObjectMapper objectMapper;
 	private final ConnectionFactory connectionFactory;
 
-	public RabbitMqSender(ConnectionFactory connectionFactory) {
-		this.connectionFactory = connectionFactory;
-	}
-
 	@Retryable(retryFor = Exception.class)
-	public void notify(RabbitMqMessage message) {
+	public <T> void send(RabbitMqMessage<T> message) {
 		try (Connection connection = connectionFactory.newConnection(); Channel channel = connection.createChannel()) {
-			channel.queueDeclare(message.getQueueName(), true, false, false, null);
-			channel.basicPublish("", message.getQueueName(), MessageProperties.PERSISTENT_TEXT_PLAIN,
-				message.toString().getBytes());
+			String queueName = message.getQueueName();
+			channel.queueDeclare(queueName, true, false, false, null);
+			channel.basicPublish("", queueName, MessageProperties.PERSISTENT_TEXT_PLAIN,
+				objectMapper.writeValueAsBytes(message.getObject()));
 		} catch (IOException | TimeoutException e) {
 			throw new RuntimeException(e);
 		}
@@ -37,7 +38,7 @@ public class RabbitMqSender {
 
 	// 재시도에 실패했을 때, 수행되는 로직
 	@Recover
-	private void onError(Exception exception, RabbitMqMessage message) {
+	private <T> void onError(Exception exception, RabbitMqMessage<T> message) {
 		log.error("RabbitMq 메시지 전송에 실패하였습니다. <메시지 = {}>", message, exception);
 	}
 }
