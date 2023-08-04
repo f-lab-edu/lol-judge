bcb593aa4d8f88a53ec0e48c15e1ef1389b3f6e1
korjun1993 <corojoon93@naver.com>
2023-08-04 22:03:25 +0900

    feat: 재판 API URL 을 뽑아내는 유틸리티

---
+++ module-api-notify/src/main/java/edu/flab/notify/util/ApiUrlUtil.java
@@ -0,0 +1,28 @@
+package edu.flab.notify.util;
+
+import org.springframework.stereotype.Component;
+
+import edu.flab.election.domain.Election;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ApiUrlUtil {
+
+	private final static String SCHEME = "https://";
+	private final static String ELECTION_API = "election";
+
+	private final ApiUrlProperties apiUrlProperties;
+
+	public String getElectionApiUrl(Election election) {
+		String host = apiUrlProperties.getHost();
+
+		if (host.endsWith("/")) {
+			host = host.substring(0, host.length() - 1);
+		}
+
+		return SCHEME + host + ":" + apiUrlProperties.getPort() + "/api/" + ELECTION_API + "/" + election.getId();
+	}
+}
