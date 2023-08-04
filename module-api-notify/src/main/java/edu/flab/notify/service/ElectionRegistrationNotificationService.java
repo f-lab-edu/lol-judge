e9d19467343e023c591c6f29f12680021d0e2166
korjun1993 <corojoon93@naver.com>
2023-08-04 22:04:29 +0900

    refactor: 일부 기능을 유틸리티로 분리, 로그레벨 수정

--- module-api-notify/src/main/java/edu/flab/notify/service/ElectionRegistrationNotificationService.java
+++ module-api-notify/src/main/java/edu/flab/notify/service/ElectionRegistrationNotificationService.java
@@ -1,23 +1,21 @@
 package edu.flab.notify.service;
 
-import java.io.IOException;
 import java.time.OffsetDateTime;
 import java.util.List;
 
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.flab.election.domain.Candidate;
 import edu.flab.election.domain.Election;
 import edu.flab.member.repository.MemberMapper;
 import edu.flab.notification.domain.Notification;
 import edu.flab.notification.repository.NotificationMapper;
+import edu.flab.notify.util.ApiUrlUtil;
 import edu.flab.rabbitmq.config.RabbitMqQueueName;
+import edu.flab.rabbitmq.util.RabbitMqMessageReader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -25,26 +23,25 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class ElectionRegistrationNotificationService {
-	@Value("${api.url}")
-	private String API_URL;
-	private final ObjectMapper objectMapper;
+	private final ApiUrlUtil apiUrlUtil;
 	private final NotificationMapper notificationMapper;
 	private final MemberMapper memberMapper;
+	private final RabbitMqMessageReader rabbitMqMessageReader;
 
 	@Transactional
 	@RabbitListener(queues = RabbitMqQueueName.ELECTION_REGISTER, ackMode = "AUTO")
 	public void saveNotificationFromRabbitMqMessage(Message message) {
-		Election election = getElectionFromMessage(message);
-		String electionModifyUrl = API_URL + "/modify/elections/" + election.getId();
+		Election election = (Election)rabbitMqMessageReader.readValue(message, Election.class);
+		String electionApiUrl = apiUrlUtil.getElectionApiUrl(election);
 
 		List<Candidate> candidates = election.getCandidates();
 
 		if (candidates == null) {
-			log.info("재판 생성 알림을 받을 대상자가 존재하지 않습니다");
+			log.warn("재판 생성 알림을 받을 대상자가 존재하지 않습니다 election = {}", election);
 			return;
 		}
 
-		candidates.forEach(c -> saveNotification(c, electionModifyUrl));
+		candidates.forEach(c -> saveNotification(c, electionApiUrl));
 	}
 
 	public void saveNotification(Candidate candidate, String electionModifyUrl) {
@@ -58,14 +55,5 @@ public class ElectionRegistrationNotificationService {
 			.contents("재판이 생성되었습니다. 자신의 의견을 작성하세요. 링크 = " + electionModifyUrl)
 			.build();
 		notificationMapper.save(notification);
-	}
-
-	private Election getElectionFromMessage(Message message) {
-		try {
-			return objectMapper.readValue(message.getBody(), Election.class);
-		} catch (IOException e) {
-			log.warn("RabbitMq 메시지 데이터를 바인딩 하는데 실패했습니다. <message = {}>", message, e);
-			throw new RuntimeException(e);
-		}
 	}
 }
