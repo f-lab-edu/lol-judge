8dd6337a05230ce5b38e9bfda962af4426fe9f6b
korjun1993 <corojoon93@naver.com>
2023-08-05 16:20:20 +0900

    fix: 예외 발생시 로깅, 재시도 실패시 예외 던지도록 수정

--- module-client-rabbitmq/src/main/java/edu/flab/rabbitmq/domain/RabbitMqSender.java
+++ module-client-rabbitmq/src/main/java/edu/flab/rabbitmq/domain/RabbitMqSender.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeoutException;
 
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.support.RetrySynchronizationManager;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -32,13 +33,16 @@ public class RabbitMqSender {
 			channel.basicPublish("", queueName, MessageProperties.PERSISTENT_TEXT_PLAIN,
 				objectMapper.writeValueAsBytes(message.getObject()));
 		} catch (IOException | TimeoutException e) {
+			log.warn("RabbitMq 메시지 전송에 실패하였습니다. <메시지 = {}>", message, e);
 			throw new RuntimeException(e);
 		}
 	}
 
 	// 재시도에 실패했을 때, 수행되는 로직
 	@Recover
-	private <T> void onError(Exception exception, RabbitMqMessage<T> message) {
-		log.error("RabbitMq 메시지 전송에 실패하였습니다. <메시지 = {}>", message, exception);
+	private <T> void onError(RuntimeException exception, RabbitMqMessage<T> message) {
+		int retryCount = RetrySynchronizationManager.getContext().getRetryCount();
+		log.error("RabbitMq 메시지 전송 재시도를 시도하였으나 실패하였습니다. <메시지 = {}> <재시도 횟수 = {}>", message, retryCount, exception);
+		throw exception;
 	}
 }
