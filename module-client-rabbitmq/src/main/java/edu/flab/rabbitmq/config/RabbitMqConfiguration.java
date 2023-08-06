b2e3db1c5094af5846ee0f4d2ad278f37cdacb2a
korjun1993 <corojoon93@naver.com>
2023-08-06 22:25:05 +0900

    refactor: RabbitTemplate 적용

--- module-client-rabbitmq/src/main/java/edu/flab/rabbitmq/config/RabbitMqConfiguration.java
+++ module-client-rabbitmq/src/main/java/edu/flab/rabbitmq/config/RabbitMqConfiguration.java
@@ -1,11 +1,17 @@
 package edu.flab.rabbitmq.config;
 
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
 
-import com.rabbitmq.client.ConnectionFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @EnableRetry
 @Configuration
@@ -25,11 +31,23 @@ public class RabbitMqConfiguration {
 
 	@Bean
 	public ConnectionFactory connectionFactory() {
-		ConnectionFactory cf = new ConnectionFactory();
-		cf.setHost(host);
+		CachingConnectionFactory cf = new CachingConnectionFactory(host, port);
 		cf.setUsername(username);
 		cf.setPassword(password);
-		cf.setPort(port);
 		return cf;
+	}
+
+	@Bean
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(messageConverter);
+		return rabbitTemplate;
+	}
+
+	@Bean
+	public MessageConverter messageConverter() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		return new Jackson2JsonMessageConverter(objectMapper);
 	}
 }
