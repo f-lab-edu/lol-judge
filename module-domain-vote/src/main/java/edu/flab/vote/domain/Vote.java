1a2d7628b0f3f741ef17c4f5447a222bff7e4f29
korjun1993 <corojoon93@naver.com>
2023-08-07 11:17:12 +0900

    feat: 투표 도메인 모듈

---
+++ module-domain-vote/src/main/java/edu/flab/vote/domain/Vote.java
@@ -0,0 +1,22 @@
+package edu.flab.vote.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Vote {
+	private Long id;
+	private Long memberId;
+	private Long electionId;
+	private Long candidateId;
+}
---
+++ module-domain-vote/src/main/java/edu/flab/vote/service/VoteFindService.java
@@ -0,0 +1,35 @@
+package edu.flab.vote.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+
+import edu.flab.vote.domain.Vote;
+import edu.flab.vote.repository.VoteMapper;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class VoteFindService {
+	private final VoteMapper voteMapper;
+
+	public Vote findVoteById(Long id) {
+		return voteMapper.findById(id)
+			.orElseThrow(() -> new NoSuchElementException("id 값으로 조회였으나 Vote 를 찾을 수 없습니다 <id = " + id + ">"));
+	}
+
+	public Vote findVoteByMemberId(Long memberId) {
+		return voteMapper.findByMemberId(memberId)
+			.orElseThrow(
+				() -> new NoSuchElementException("memberId 값으로 조회였으나 Vote 를 찾을 수 없습니다 <memberId = " + memberId + ">"));
+	}
+
+	public List<Vote> findAllVotesByElectionId(Long electionId) {
+		return voteMapper.findByElectionId(electionId);
+	}
+
+	public boolean hasVotedBefore(Long memberId) {
+		return voteMapper.findByMemberId(memberId).isPresent();
+	}
+}
