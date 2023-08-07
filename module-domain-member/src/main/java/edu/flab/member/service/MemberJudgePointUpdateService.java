6cf36c8f7f7fed288b3ad9843cb1d9d49e7b603c
korjun1993 <corojoon93@naver.com>
2023-08-07 11:13:31 +0900

    feat: 포인트 더하기, 빼기 기능

--- module-domain-member/src/main/java/edu/flab/member/service/MemberJudgePointUpdateService.java
+++ module-domain-member/src/main/java/edu/flab/member/service/MemberJudgePointUpdateService.java
@@ -1,11 +1,10 @@
 package edu.flab.member.service;
 
-import java.util.NoSuchElementException;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import edu.flab.member.domain.Member;
+import edu.flab.member.dto.MemberJudgePointCalcDto;
 import edu.flab.member.dto.MemberJudgePointUpdateDto;
 import edu.flab.member.event.MemberRankScoreUpdateEventPublisher;
 import edu.flab.member.repository.MemberMapper;
@@ -18,22 +17,32 @@ import lombok.extern.slf4j.Slf4j;
 public class MemberJudgePointUpdateService {
 
 	private final MemberMapper memberMapper;
+	private final MemberFindService memberFindService;
 	private final MemberRankScoreUpdateEventPublisher memberRankScoreUpdateEventPublisher;
 
 	@Transactional
-	public Member updateJudgePoint(MemberJudgePointUpdateDto dto) {
-		Member member = memberMapper.findActiveMemberById(dto.getId())
-			.orElseThrow(() -> new NoSuchElementException("회원이 존재하지 않습니다."));
-
+	public Member updateJudgePoint(Member member, int updatedJudgePoint) {
 		int beforeUpdate = member.getJudgePoint();
 
-		memberMapper.updateJudgePoint(dto);
-		member.updateJudgePoint(dto.getJudgePoint());
+		memberMapper.updateJudgePoint(new MemberJudgePointUpdateDto(member.getId(), updatedJudgePoint));
+		member.updateJudgePoint(updatedJudgePoint);
 
 		log.info("JudgePoint 업데이트 완료 <회원 이메일: {}> <변경 내역: {} → {}>", member.getEmail(), beforeUpdate,
-			member.getJudgePoint());
+			updatedJudgePoint);
 
 		memberRankScoreUpdateEventPublisher.publishEvent(member);
 		return member;
+	}
+
+	@Transactional
+	public Member minusJudgePoint(MemberJudgePointCalcDto dto) {
+		Member member = memberFindService.findActiveMember(dto.getId());
+		return updateJudgePoint(member, member.getJudgePoint() - dto.getValue());
+	}
+
+	@Transactional
+	public Member plusJudgePoint(MemberJudgePointCalcDto dto) {
+		Member member = memberFindService.findActiveMember(dto.getId());
+		return updateJudgePoint(member, member.getJudgePoint() + dto.getValue());
 	}
 }
