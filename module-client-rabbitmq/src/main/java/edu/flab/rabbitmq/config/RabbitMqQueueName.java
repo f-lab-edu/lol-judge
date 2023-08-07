63ba86d4c0c4321cac6731fafa460066f40cbf0a
korjun1993 <corojoon93@naver.com>
2023-08-07 15:56:18 +0900

    feat: 재판 종료시 판결 기능

--- module-client-rabbitmq/src/main/java/edu/flab/rabbitmq/config/RabbitMqQueueName.java
+++ module-client-rabbitmq/src/main/java/edu/flab/rabbitmq/config/RabbitMqQueueName.java
@@ -3,4 +3,5 @@ package edu.flab.rabbitmq.config;
 public class RabbitMqQueueName {
 	public static final String ELECTION_REGISTER = "ELECTION_REGISTER";
 	public static final String ELECTION_IN_PROGRESS = "ELECTION_IN_PROGRESS";
+	public static final String ELECTION_FINISHED = "ELECTION_FINISHED";
 }
