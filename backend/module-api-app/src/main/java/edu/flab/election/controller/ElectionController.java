c389eef3468e85ee75a4a2ea32e7dfff38437767
korjun1993 <corojoon93@naver.com>
2023-09-07 05:40:28 +0900

    feat: 재판 상세 조회 API

--- backend/module-api-app/src/main/java/edu/flab/election/controller/ElectionController.java
+++ backend/module-api-app/src/main/java/edu/flab/election/controller/ElectionController.java
@@ -1,15 +1,22 @@
 package edu.flab.election.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.flab.election.dto.ElectionDetailResponseDto;
 import edu.flab.election.dto.ElectionRegisterRequestDto;
 import edu.flab.election.dto.ElectionRegisterResponseDto;
+import edu.flab.election.service.ElectionDetailService;
 import edu.flab.election.service.ElectionRegisterService;
+import edu.flab.member.domain.Member;
+import edu.flab.web.annotation.Login;
 import edu.flab.web.response.SuccessResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,12 +25,21 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ElectionController {
 
-	private final ElectionRegisterService electionAddService;
+	private final ElectionRegisterService electionRegisterService;
+	private final ElectionDetailService electionDetailService;
 
 	@ResponseStatus(HttpStatus.OK)
-	@PostMapping("/api/election")
-	public SuccessResponse<ElectionRegisterResponseDto> addElection(@RequestBody ElectionRegisterRequestDto dto) {
-		ElectionRegisterResponseDto response = electionAddService.register(dto);
+	@PostMapping("/elections")
+	public SuccessResponse<ElectionRegisterResponseDto> addElection(@Login Member member,
+		@RequestBody @Valid ElectionRegisterRequestDto dto) {
+		ElectionRegisterResponseDto response = electionRegisterService.register(member.getEmail(), dto);
 		return SuccessResponse.of(response);
+	}
+
+	@ResponseStatus(HttpStatus.OK)
+	@GetMapping("/elections/{electionId}")
+	public SuccessResponse<ElectionDetailResponseDto> findElectionDetail(@PathVariable Long electionId) {
+		ElectionDetailResponseDto electionDetail = electionDetailService.findElectionDetail(electionId);
+		return SuccessResponse.of(electionDetail);
 	}
 }
---
+++ backend/module-api-app/src/main/java/edu/flab/election/dto/ElectionDetailResponseDto.java
@@ -0,0 +1,33 @@
+package edu.flab.election.dto;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+import edu.flab.election.config.ElectionConstant;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ElectionDetailResponseDto {
+	private Long id;
+
+	@Length(max = 50)
+	private String youtubeUrl;
+
+	@Range(min = ElectionConstant.MIN_COST, max = ElectionConstant.MAX_COST)
+	private int cost;
+
+	private String hostChampion;
+
+	private String hostOpinion;
+
+	private String participantChampion;
+
+	private String participantOpinion;
+}
