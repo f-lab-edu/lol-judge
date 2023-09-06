ded8a42155bf24e583a757865c3c8e0ae9242472
김호준 <45716246+korjun1993@users.noreply.github.com>
2023-09-07 06:12:18 +0900

    테스트 컨테이너 도입 (#58)

--- backend/module-api-notify/src/test/java/edu/flab/notify/service/ElectionNotificationServiceTest.java
+++ backend/module-api-notify/src/test/java/edu/flab/notify/service/ElectionNotificationServiceTest.java
@@ -2,6 +2,7 @@ package edu.flab.notify.service;
 
 import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import edu.flab.notify.listener.ElectionNotifyListener;
 import edu.flab.rabbitmq.config.RabbitMqQueueName;
@@ -19,6 +25,7 @@ import edu.flab.rabbitmq.message.RabbitMqMessage;
 
 @Tag("integration")
 @SpringBootTest
+@Testcontainers
 @ExtendWith(MockitoExtension.class)
 class ElectionNotificationServiceTest {
 
@@ -27,6 +34,22 @@ class ElectionNotificationServiceTest {
 
 	@SpyBean
 	private ElectionNotifyListener sut;
+
+	@Container
+	private static final RabbitMQContainer rabbitmq = new RabbitMQContainer("rabbitmq:latest").withExposedPorts(5672, 15672);
+
+	@BeforeEach
+	void setUp() {
+		rabbitmq.start();
+	}
+
+	@DynamicPropertySource
+	static void registerProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.rabbitmq.host", rabbitmq::getHost);
+		registry.add("spring.rabbitmq.port", rabbitmq::getAmqpPort);
+		registry.add("spring.rabbitmq.username", rabbitmq::getAdminUsername);
+		registry.add("spring.rabbitmq.password", rabbitmq::getAdminPassword);
+	}
 
 	@Test
 	@DisplayName("메시지 큐에 재판이 생성됐다는 데이터가 삽입되면, 재판 당사자들에게 전송될 알림 정보가 데이터베이스에 저장된다")
