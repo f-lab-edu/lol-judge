82b52fff50bc2322b11e999ccb197c973981abc2
korjun1993 <corojoon93@naver.com>
2023-09-07 22:25:10 +0900

    feat: 재판 내용 수정

--- backend/module-api-app/src/main/java/edu/flab/election/controller/ElectionController.java
+++ backend/module-api-app/src/main/java/edu/flab/election/controller/ElectionController.java
@@ -4,14 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.flab.election.dto.ElectionDetailResponseDto;
+import edu.flab.election.dto.ElectionEditRequestDto;
 import edu.flab.election.dto.ElectionRegisterRequestDto;
 import edu.flab.election.dto.ElectionRegisterResponseDto;
 import edu.flab.election.service.ElectionDetailService;
+import edu.flab.election.service.ElectionEditService;
 import edu.flab.election.service.ElectionRegisterService;
 import edu.flab.member.domain.Member;
 import edu.flab.web.annotation.Login;
@@ -27,6 +30,7 @@ public class ElectionController {
 
 	private final ElectionRegisterService electionRegisterService;
 	private final ElectionDetailService electionDetailService;
+	private final ElectionEditService electionEditService;
 
 	@ResponseStatus(HttpStatus.OK)
 	@PostMapping("/elections")
@@ -41,5 +45,12 @@ public class ElectionController {
 	public SuccessResponse<ElectionDetailResponseDto> findElectionDetail(@PathVariable Long electionId) {
 		ElectionDetailResponseDto electionDetail = electionDetailService.findElectionDetail(electionId);
 		return SuccessResponse.of(electionDetail);
+	}
+
+	@ResponseStatus(HttpStatus.OK)
+	@PutMapping("/elections")
+	public SuccessResponse<Void> updateElection(@RequestBody @Valid ElectionEditRequestDto dto) {
+		electionEditService.update(dto);
+		return SuccessResponse.ok();
 	}
 }
